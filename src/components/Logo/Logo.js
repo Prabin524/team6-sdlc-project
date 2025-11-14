450c36c918185e85c794696710d34803eb43cf0f
Prabin524 <prabinprasainofficial@gmail.com>
2025-11-14 14:27:23 -0500

    Some components added | #26

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,10 @@
+// import logoDark from 'assets/images/logo-dark.svg';
+import logo from 'assets/images/Employee.svg';
+
+// ==============================|| LOGO SVG ||============================== //
+
+const Logo = () => {
+  return <img src={logo} alt="Employee" width="180" height={'150'} />;
+};
+
+export default Logo;
